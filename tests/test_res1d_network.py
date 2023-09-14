48e9f499027aa399d67e5111016d168eb483d7f1
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2023-09-14 15:30:28 +0200

    Add unit tests for dotnet datetime conversion

--- tests/test_res1d_network.py
+++ tests/test_res1d_network.py
@@ -3,9 +3,11 @@ import pytest
 import numpy as np
 import pandas as pd
 
+import System
+
 from mikeio1d.custom_exceptions import NoDataForQuery, InvalidQuantity
 from mikeio1d.res1d import Res1D, mike1d_quantities, QueryDataReach, QueryDataNode
-from mikeio1d.dotnet import to_numpy
+from mikeio1d.dotnet import to_numpy, from_dotnet_datetime, to_dotnet_datetime
 
 
 @pytest.fixture
@@ -44,16 +46,15 @@ def test_repr(test_file):
     res1d = test_file
     res1d_repr = res1d.__repr__()
     res1d_repr_ref = (
-        '<mikeio1d.Res1D>\n' +
-        'Start time: 1994-08-07 16:35:00\n' +
-        'End time: 1994-08-07 18:35:00\n'
-        '# Timesteps: 110\n' +
-        '# Catchments: 0\n' +
-        '# Nodes: 119\n' +
-        '# Reaches: 118\n' +
-        '# Globals: 0\n'
-        '0 - WaterLevel <m>\n' +
-        '1 - Discharge <m^3/s>'
+        "<mikeio1d.Res1D>\n"
+        + "Start time: 1994-08-07 16:35:00\n"
+        + "End time: 1994-08-07 18:35:00\n"
+        "# Timesteps: 110\n"
+        + "# Catchments: 0\n"
+        + "# Nodes: 119\n"
+        + "# Reaches: 118\n"
+        + "# Globals: 0\n"
+        "0 - WaterLevel <m>\n" + "1 - Discharge <m^3/s>"
     )
     assert res1d_repr == res1d_repr_ref
 
@@ -67,11 +68,14 @@ def test_data_item_dicts(test_file):
     assert len(res1d.global_data) == 0
 
 
-@pytest.mark.parametrize("query,expected", [
-    (QueryDataReach("WaterLevel", "104l1", 34.4131), True),
-    (QueryDataReach("WaterLevel", "104l1", 42424242), False),
-    (QueryDataReach("WaterLevel", "wrong_reach_name", 34.4131), False)
-])
+@pytest.mark.parametrize(
+    "query,expected",
+    [
+        (QueryDataReach("WaterLevel", "104l1", 34.4131), True),
+        (QueryDataReach("WaterLevel", "104l1", 42424242), False),
+        (QueryDataReach("WaterLevel", "wrong_reach_name", 34.4131), False),
+    ],
+)
 def test_valid_reach_data_queries(test_file, query, expected):
     res1d = test_file
 
@@ -86,23 +90,29 @@ def test_valid_reach_data_queries(test_file, query, expected):
             assert res1d.read(query)
 
 
-@pytest.mark.parametrize("query,expected_max", [
-    (QueryDataReach("WaterLevel", "104l1", 34.4131), 197.046),
-    (QueryDataReach("WaterLevel", "9l1", 10), 195.165),
-    (QueryDataReach("Discharge", "100l1", 23.8414), 0.1),
-    (QueryDataReach("Discharge", "9l1", 5), 0.761)
-])
+@pytest.mark.parametrize(
+    "query,expected_max",
+    [
+        (QueryDataReach("WaterLevel", "104l1", 34.4131), 197.046),
+        (QueryDataReach("WaterLevel", "9l1", 10), 195.165),
+        (QueryDataReach("Discharge", "100l1", 23.8414), 0.1),
+        (QueryDataReach("Discharge", "9l1", 5), 0.761),
+    ],
+)
 def test_read_reach_with_queries(test_file, query, expected_max):
     data = test_file.read(query)
     assert pytest.approx(round(data.max().values[0], 3)) == expected_max
 
 
-@pytest.mark.parametrize("quantity,reach_id,chainage,expected_max", [
-    ("WaterLevel", "104l1", 34.4131, 197.046),
-    ("WaterLevel", "9l1", 10, 195.165),
-    ("Discharge", "100l1", 23.8414, 0.1),
-    ("Discharge", "9l1", 5, 0.761)
-])
+@pytest.mark.parametrize(
+    "quantity,reach_id,chainage,expected_max",
+    [
+        ("WaterLevel", "104l1", 34.4131, 197.046),
+        ("WaterLevel", "9l1", 10, 195.165),
+        ("Discharge", "100l1", 23.8414, 0.1),
+        ("Discharge", "9l1", 5, 0.761),
+    ],
+)
 def test_read_reach(test_file, quantity, reach_id, chainage, expected_max):
     data = test_file.query.GetReachValues(reach_id, chainage, quantity)
     data = to_numpy(data)
@@ -110,10 +120,10 @@ def test_read_reach(test_file, quantity, reach_id, chainage, expected_max):
     assert pytest.approx(actual_max) == expected_max
 
 
-@pytest.mark.parametrize("quantity,node_id,expected_max", [
-    ("WaterLevel", "1", 195.669),
-    ("WaterLevel", "2", 195.823)
-])
+@pytest.mark.parametrize(
+    "quantity,node_id,expected_max",
+    [("WaterLevel", "1", 195.669), ("WaterLevel", "2", 195.823)],
+)
 def test_read_node(test_file, quantity, node_id, expected_max):
     data = test_file.query.GetNodeValues(node_id, quantity)
     data = to_numpy(data)
@@ -128,9 +138,31 @@ def test_time_index(test_file):
 def test_start_time(test_file):
     assert test_file.start_time == test_file.time_index.min()
 
+
+def test_from_dotnet_datetime_preserves_millisecond_precision():
+    dtstr = "2021-01-01 00:00:00.123"
+
+    dotnet_dt = System.DateTime.Parse(dtstr)
+    assert dotnet_dt.Millisecond == 123
+    py_dt = from_dotnet_datetime(dotnet_dt)
+    # python datetime doesn't have a millisecond property so we use microsecond
+    assert py_dt.microsecond == 123000
+
+
+# Write the same test as above, but in the reverse direction
+def test_to_dotnet_datetime_preserves_millisecond_precision():
+    dtstr = "2021-01-01 00:00:00.123"
+
+    py_dt = pd.to_datetime(dtstr)
+    assert py_dt.microsecond == 123000
+    dotnet_dt = to_dotnet_datetime(py_dt)
+    assert dotnet_dt.Millisecond == 123
+
+
 def test_time_index_microseconds(test_file):
     df = test_file.read()
-    assert df.index.microsecond.unique().size  > 1
+    assert df.index.microsecond.unique().size > 1
+
 
 def test_get_node_values(test_file):
     values = test_file.get_node_values("1", "WaterLevel")
@@ -159,11 +191,15 @@ def test_dotnet_methods(test_file):
     res1d.data.ResultSpecs
     res1d.data.Nodes
     res1d.query.GetNodeValues("1", "WaterLevel")
-    res1d.query.GetReachValue("9l1", 5, "WaterLevel", res1d.data.StartTime)  # must be dotnet datetime
+    res1d.query.GetReachValue(
+        "9l1", 5, "WaterLevel", res1d.data.StartTime
+    )  # must be dotnet datetime
     res1d.query.GetReachValues("9l1", 5, "WaterLevel")
     res1d.query.GetReachEndValues("9l1", "WaterLevel")  # avoid specifying chainage
     res1d.query.GetReachStartValues("9l1", "WaterLevel")  # avoid specifying chainage
-    res1d.query.GetReachSumValues("9l1", "WaterLevel")  # useful for summing volume in reach (all grid points)
+    res1d.query.GetReachSumValues(
+        "9l1", "WaterLevel"
+    )  # useful for summing volume in reach (all grid points)
 
 
 def test_res1d_filter(test_file_path):
