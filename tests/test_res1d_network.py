b709cef1e1d449ad2f705d02d9209da7ccceecb0
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2023-10-18 11:05:39 +0200

    Node changes should warn but not break

--- tests/test_res1d_network.py
+++ tests/test_res1d_network.py
@@ -280,3 +280,18 @@ def test_structure_reach_attributes(test_file):
 
     max_discharge = round(df.max().max(), 3)
     assert pytest.approx(max_discharge) == 1.491
+
+
+def test_nodes_dict_access_maintains_backwards_compatibility(res1d_network):
+    with pytest.warns(UserWarning):
+        node = res1d_network.result_network.nodes["1"]
+        assert node.GroundLevel == pytest.approx(197.07000732421875)
+        assert node.BottomLevel == pytest.approx(195.0500030517578)
+        assert node.XCoordinate == pytest.approx(-687934.6000976562)
+
+
+def test_node_node_property_maintains_backwards_compatibility(res1d_network):
+    node = res1d_network.result_network.nodes.n_1
+    assert node.node.GroundLevel == pytest.approx(197.07000732421875)
+    assert node.node.BottomLevel == pytest.approx(195.0500030517578)
+    assert node.node.XCoordinate == pytest.approx(-687934.6000976562)
