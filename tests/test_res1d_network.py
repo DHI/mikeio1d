bb5a04592c32c25e2b72c1a15780d6fc89db12ec
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2023-10-18 16:46:28 +0200

    Test to not break current ResultReach indexing

--- tests/test_res1d_network.py
+++ tests/test_res1d_network.py
@@ -295,3 +295,17 @@ def test_node_node_property_maintains_backwards_compatibility(res1d_network):
     assert node.node.GroundLevel == pytest.approx(197.07000732421875)
     assert node.node.BottomLevel == pytest.approx(195.0500030517578)
     assert node.node.XCoordinate == pytest.approx(-687934.6000976562)
+
+
+def test_reaches_dict_access_maintains_backwards_compatibility(
+    res1d_network, res1d_river_network
+):
+    with pytest.warns(UserWarning):
+        # Indexing reaches could return a single dotnet reach
+        reach = res1d_network.result_network.reaches["100l1"]
+        assert reach.Name == "100l1"
+        assert reach.Length == pytest.approx(47.6827148432828)
+        # Or it could include multiple reaches
+        reach = res1d_river_network.result_network.reaches["river"]
+        assert iter(reach), "Should be iterable where there is several subreaches"
+        assert reach[0].Id == "river-12"
