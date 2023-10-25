21ad07cb7948485388aa3a2e06dc87dcb0d415ce
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2023-10-25 16:17:12 +0200

    Update doc landing page and start outline

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,16 @@ extensions = [
     "myst_parser",
 ]
 
+autodoc_typehints = "none"
+
+# Napoleon configurations
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_use_param = False
+napoleon_use_rtype = False
+napoleon_preprocess_types = True
+
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
@@ -34,3 +44,5 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 # html_logo = "..\images\logo\MIKE-IO-1D-Logo-Pos-RGB-nomargin.png"
 # autodoc_typehints = "none"
+
+myst_heading_anchors = 2
