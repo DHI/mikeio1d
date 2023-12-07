69e9fb22a1c09c1b5c85a45530a31f2a1fd7979e
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2023-12-07 13:24:01 +0100

    Try reach geometry as utility class instead, along with reach points

--- mikeio1d/result_network/geometry.py
+++ mikeio1d/result_network/geometry.py
@@ -1,3 +1,11 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from enum import Enum
+from functools import cache
+from typing import Iterable, List
+
+import numpy as np
 import shapely
 
 
@@ -45,8 +53,8 @@ def geometry_from_reaches(dotnet_reaches) -> shapely.LineString:
 
     Parameters
     ----------
-    dotnet_reach : iterable of IRes1DReach
 
+    dotnet_reach : iterable of IRes1DReach
     Returns
     -------
     shapely.LineString
@@ -55,6 +63,146 @@ def geometry_from_reaches(dotnet_reaches) -> shapely.LineString:
     geometry = shapely.MultiLineString(linestrings)
     geometry = shapely.line_merge(geometry)
     return geometry
+
+
+class ReachPointType(Enum):
+    DIGIPOINT = 0
+    GRIDPOINT = 1
+
+
+@dataclass(frozen=True, order=True)
+class ReachPoint:
+    """
+    A point on a reach, either a digipoint or gridpoint.
+    Parameters
+    ----------
+    point_type : ReachPointType
+        Either DIGIPOINT or GRIDPOINT
+    chainage : float
+        Chainage along the reach. Need not match geometric distance.
+    x : float
+        X coordinate
+    y : float
+        Y coordinate
+    z : float
+        Z coordinate
+    """
+
+    point_type: ReachPointType = field(compare=False)
+    chainage: float
+    x: float
+    y: float
+    z: float
+
+    def is_digipoint(self):
+        return self.point_type == ReachPointType.DIGIPOINT
+
+    def is_gridpoint(self):
+        return self.point_type == ReachPointType.GRIDPOINT
+
+    def to_shapely(self) -> shapely.Point:
+        return shapely.Point(self.x, self.y)
+
+    @staticmethod
+    def from_digipoint(digipoint):
+        return ReachPoint(
+            ReachPointType.DIGIPOINT,
+            digipoint.M,
+            digipoint.X,
+            digipoint.Y,
+            digipoint.Z,
+        )
+
+    @staticmethod
+    def from_gridpoint(gridpoint):
+        return ReachPoint(
+            ReachPointType.GRIDPOINT,
+            gridpoint.Chainage,
+            gridpoint.X,
+            gridpoint.Y,
+            gridpoint.Z,
+        )
+
+
+class ReachGeometry:
+    def __init__(self, points: List[ReachPoint]):
+        self._points = sorted(points)
+
+    @staticmethod
+    def from_dotnet_reaches(dotnet_reaches):
+        if not isinstance(dotnet_reaches, Iterable):
+            dotnet_reaches = [dotnet_reaches]
+
+        points = []
+        for reach in dotnet_reaches:
+            points.extend([ReachPoint.from_digipoint(dp) for dp in reach.DigiPoints])
+            points.extend([ReachPoint.from_gridpoint(gp) for gp in reach.GridPoints])
+
+        return ReachGeometry(points)
+
+    @property
+    def chainages(self) -> List[float]:
+        return [p.chainage for p in self._get_unique_points()]
+
+    @property
+    def points(self) -> List[ReachPoint]:
+        return self._points
+
+    @property
+    def digipoints(self) -> List[ReachPoint]:
+        return [p for p in self.points if p.is_digipoint()]
+
+    @property
+    def gridpoints(self) -> List[ReachPoint]:
+        return [p for p in self.points if p.is_gridpoint()]
+
+    @property
+    def length(self) -> float:
+        return self.chainages[-1] - self.chainages[0]
+
+    def to_shapely(self) -> shapely.LineString:
+        points = self._get_unique_points()
+        xy = [(p.x, p.y) for p in points]
+        return shapely.LineString(xy)
+
+    def chainage_to_geometric_distance(self, chainage: float) -> float:
+        """Convert chainage to geometric distance."""
+        chainages = self.chainages
+        distances = self._get_distances()
+        if chainage < chainages[0] or chainage > chainages[-1]:
+            raise ValueError(
+                f"Chainage of {chainage} outside reach range of {chainages[0]} to {chainages[-1]}"
+            )
+        distance = float(np.interp(chainage, chainages, distances))
+        return distance
+
+    def chainage_from_geometric_distance(self, geometric_distance: float) -> float:
+        """Convert geometric distance to chainage."""
+        chainages = self.chainages
+        distances = self._get_distances()
+        if geometric_distance < distances[0] or geometric_distance > distances[-1]:
+            raise ValueError(
+                f"Geometric distance of {geometric_distance} outside reach range of {distances[0]} to {distances[-1]}"
+            )
+        chainage = float(np.interp(geometric_distance, distances, chainages))
+        return chainage
+
+    @cache
+    def _get_unique_points(self) -> List[ReachPoint]:
+        return sorted(list(set(self._points)))
+
+    @cache
+    def _get_distances(self) -> List[float]:
+        points = self._get_unique_points()
+        distances = []
+        total_distance = 0.0
+        prev_point = points[0]
+        for point in points:
+            distance = point.to_shapely().distance(prev_point.to_shapely())
+            total_distance += distance
+            distances.append(total_distance)
+            prev_point = point
+        return distances
 
 
 def geometry_from_catchment(dotnet_catchment) -> shapely.Polygon:
--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,4 +1,5 @@
 import pytest
+import random
 
 import shapely
 
@@ -6,6 +7,124 @@ from mikeio1d.result_network.geometry import geometry_from_node
 from mikeio1d.result_network.geometry import geometry_from_reach
 from mikeio1d.result_network.geometry import geometry_from_reaches
 from mikeio1d.result_network.geometry import geometry_from_catchment
+from mikeio1d.result_network.geometry import ReachGeometry
+from mikeio1d.result_network.geometry import ReachPoint
+from mikeio1d.result_network.geometry import ReachPointType
+
+
+class TestReachPoint:
+    def test_reach_point_basic(self):
+        p1 = ReachPoint(ReachPointType.DIGIPOINT, 0, 0, 0, 0)
+        p2 = ReachPoint(ReachPointType.GRIDPOINT, 0, 0, 0, 0)
+        p3 = ReachPoint(ReachPointType.GRIDPOINT, 10, 0, 0, 0)
+        assert p1 == p2, "Should be equal if chainage and coordinates are equal"
+        assert p3 > p1, "Should be greater if chainage is greater"
+        assert p1 < p3, "Should be less if chainage is less"
+        points = [p3, p1, p2]
+        assert sorted(points) == [p1, p2, p3], "Should sort by chainage."
+        assert sorted(list(set(points))) == [p1, p3], "Should remove duplicates."
+
+    def test_reach_point_to_shapely(self):
+        p = ReachPoint(ReachPointType.DIGIPOINT, 0, 0, 0, 0)
+        g = p.to_shapely()
+        assert isinstance(g, shapely.Point)
+        assert g.x == 0
+        assert g.y == 0
+
+
+class TestReachGeometry:
+    @pytest.fixture
+    def reach_geometry(self):
+        points = [
+            ReachPoint(ReachPointType.DIGIPOINT, 0, 0, 0, 0),
+            ReachPoint(ReachPointType.GRIDPOINT, 0, 0, 0, 0),
+            ReachPoint(ReachPointType.GRIDPOINT, 5, 0.5, 0.5, 0.5),
+            ReachPoint(ReachPointType.GRIDPOINT, 10, 1, 1, 1),
+            ReachPoint(ReachPointType.DIGIPOINT, 10, 1, 1, 1),
+        ]
+        random.shuffle(points)
+        return ReachGeometry(points)
+
+    def test_reach_geometry_basic(self, reach_geometry):
+        assert len(reach_geometry.points) == 5
+        assert len(reach_geometry.digipoints) == 2
+        assert len(reach_geometry.gridpoints) == 3
+        assert reach_geometry.chainages == [0, 5, 10]
+        assert reach_geometry.length == 10
+        prev_chainage = reach_geometry.points[0].chainage
+        for p in reach_geometry.points:
+            assert p.chainage >= prev_chainage, "Chainages should be sorted in ascending order."
+            prev_chainage = p.chainage
+
+    def test_reach_geometry_to_shapely(self, reach_geometry):
+        g = reach_geometry.to_shapely()
+        assert isinstance(g, shapely.LineString)
+        assert len(g.coords) == 3, "Should only be 3 unique points"
+        assert g.coords[0] == (0, 0)
+        assert g.coords[1] == (0.5, 0.5)
+        assert g.coords[2] == (1, 1)
+        assert g.length == pytest.approx(2**0.5)
+
+    def test_reach_geometry_chainage_to_geometric_distance(self, reach_geometry):
+        assert reach_geometry.chainage_to_geometric_distance(0) == 0
+        assert reach_geometry.chainage_to_geometric_distance(5) == pytest.approx(
+            (0.5**2 + 0.5**2) ** 0.5
+        )
+        assert reach_geometry.chainage_to_geometric_distance(10) == pytest.approx(
+            (1**2 + 1**2) ** 0.5
+        )
+
+    def test_reach_chainage_from_geometric_distance(self, reach_geometry):
+        assert reach_geometry.chainage_from_geometric_distance(0) == 0
+        assert reach_geometry.chainage_from_geometric_distance((0.5**2 + 0.5**2) ** 0.5) == 5
+        assert reach_geometry.chainage_from_geometric_distance((1**2 + 1**2) ** 0.5) == 10
+
+    def test_from_dotnet_reaches(self, river_reach):
+        g = ReachGeometry.from_dotnet_reaches(river_reach.reaches)
+        expected_n_gridpoints = sum([reach.GridPoints.Count for reach in river_reach.reaches])
+        assert len(g.gridpoints) == expected_n_gridpoints
+        expected_n_digipoints = sum([reach.DigiPoints.Count for reach in river_reach.reaches])
+        assert len(g.digipoints) == expected_n_digipoints
+        assert len(g.points) == expected_n_gridpoints + expected_n_digipoints
+        dotnet_gp_start = river_reach.reaches[0].GridPoints[0]
+        assert g.gridpoints[0].x == pytest.approx(dotnet_gp_start.X)
+        assert g.gridpoints[0].y == pytest.approx(dotnet_gp_start.Y)
+        assert g.gridpoints[0].z == pytest.approx(dotnet_gp_start.Z)
+        assert g.gridpoints[0].chainage == pytest.approx(dotnet_gp_start.Chainage)
+        dotnet_dp_start = river_reach.reaches[0].DigiPoints[0]
+        assert g.digipoints[0].x == pytest.approx(dotnet_dp_start.X)
+        assert g.digipoints[0].y == pytest.approx(dotnet_dp_start.Y)
+        assert g.digipoints[0].z == pytest.approx(dotnet_dp_start.Z)
+        assert g.digipoints[0].chainage == pytest.approx(dotnet_dp_start.M)
+        dotnet_gp_end = list(river_reach.reaches[-1].GridPoints)[-1]
+        assert g.gridpoints[-1].x == pytest.approx(dotnet_gp_end.X)
+        assert g.gridpoints[-1].y == pytest.approx(dotnet_gp_end.Y)
+        assert g.gridpoints[-1].z == pytest.approx(dotnet_gp_end.Z)
+        assert g.gridpoints[-1].chainage == pytest.approx(dotnet_gp_end.Chainage)
+        dotnet_dp_end = list(river_reach.reaches[-1].DigiPoints)[-1]
+        assert g.digipoints[-1].x == pytest.approx(dotnet_dp_end.X)
+        assert g.digipoints[-1].y == pytest.approx(dotnet_dp_end.Y)
+        assert g.digipoints[-1].z == pytest.approx(dotnet_dp_end.Z)
+        assert g.digipoints[-1].chainage == pytest.approx(dotnet_dp_end.M)
+        prev_chainage = g.points[0].chainage
+        for p in g.points:
+            assert p.chainage >= prev_chainage, "Chainages should be sorted in ascending order."
+            prev_chainage = p.chainage
+
+    def test_reaches_point_interpolation_matches_mikeplus(self, river_reach):
+        g = ReachGeometry.from_dotnet_reaches(river_reach.reaches)
+        shape = g.to_shapely()
+        # from MIKE+ chainage_points
+        expected_coords = {
+            53100: (385711.47339682, 5716561.25558023),
+            54809.23: (386779.521238822, 5715252.98133197),
+            54811.75: (386781.208762897, 5715251.10743646),
+            55124.2276598819: (386997.40643911, 5715025.5383054),
+        }
+        for expected_chainage, expected_xy in expected_coords.items():
+            distance = g.chainage_to_geometric_distance(expected_chainage)
+            interp_point = shape.interpolate(distance)
+            assert (interp_point.x, interp_point.y) == pytest.approx(expected_xy)
 
 
 def test_geometry_from_node(node):
@@ -24,29 +143,6 @@ def test_geometry_from_reach(reach):
     assert x == pytest.approx([-687887.6008911133, -687907.999206543])
     assert y == pytest.approx([-1056368.9006958008, -1056412.0])
     assert g.length == pytest.approx(47.6827148432828)
-
-
-def test_geometry_from_reaches(river_reach):
-    reach = river_reach
-    g = geometry_from_reaches(reach.reaches)
-    assert isinstance(g, shapely.LineString)
-    # from MIKE+ chainage_points
-    expected_coords = {
-        53100: (385711.47339682, 5716561.25558023),
-        54809.23: (386779.521238822, 5715252.98133197),
-        54811.75: (386781.208762897, 5715251.10743646),
-        55124.23: (386997.40643911, 5715025.5383054),
-    }
-    for chainage, expected_coord in expected_coords.items():
-        distance = chainage - 53100
-        assert g.interpolate(distance).coords[0] == pytest.approx(expected_coord)
-
-    # TODO: not sure why this fails
-    #       - the length calculation seems to be different for mike1d and geopandas
-    #       - I suspect it's the difference between cartographic vs ellipsoidal distance
-    #       - MIKE+ may have mapping from digipoints to chainage
-    expected_length = 55124.23 - 53100
-    assert expected_length == pytest.approx(g.length)
 
 
 def test_geometry_from_catchment(catchment):
