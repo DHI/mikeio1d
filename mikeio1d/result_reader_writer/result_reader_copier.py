6d30001833f2ce8c1cb926e8d34b82f90bdf7f6d
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-01-09 16:21:56 +0100

    Minimally update to enable reading to multiindex dataframe

--- mikeio1d/result_reader_writer/result_reader_copier.py
+++ mikeio1d/result_reader_writer/result_reader_copier.py
@@ -1,9 +1,18 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List
+    from ..result_query import QueryData
+
 import numpy as np
 import pandas as pd
 
 from ..dotnet import pythonnet_implementation as impl
 from ..various import NAME_DELIMITER
 from .result_reader import ResultReader
+from ..quantities import TimeseriesId
 
 from System import IntPtr
 
@@ -45,21 +54,21 @@ class ResultReaderCopier(ResultReader):
 
         self.result_data_copier = ResultDataCopier(self.data)
 
-    def read(self, queries=None):
-        column_names = []
+    def read(self, queries: List[QueryData] = None):
+        timerseries_ids: List[TimeseriesId] = []
         data_entries = self.result_data_copier.GetEmptyDataEntriesList()
         for query in queries:
-            query.add_to_data_entries(self.res1d, data_entries, column_names)
+            query.add_to_data_entries(self.res1d, data_entries, timerseries_ids)
 
-        df = self.create_data_frame(data_entries, column_names)
+        df = self.create_data_frame(data_entries, timerseries_ids)
         return df
 
     def read_all(self):
-        data_entries, column_names = self.get_all_data_entries_and_column_names()
-        df = self.create_data_frame(data_entries, column_names)
+        data_entries, timeseries_ids = self.get_all_data_entries_and_timeseries_ids()
+        df = self.create_data_frame(data_entries, timeseries_ids)
         return df
 
-    def create_data_frame(self, data_entries, column_names):
+    def create_data_frame(self, data_entries, timeseries_ids: List[TimeseriesId]):
         number_of_timesteps = self.data.NumberOfTimeSteps
         number_of_items = len(data_entries)
 
@@ -70,15 +79,17 @@ class ResultReaderCopier(ResultReader):
         data_pointer_net = IntPtr(data_pointer)
         self.result_data_copier.CopyData(data_pointer_net, data_entries)
 
-        df = pd.DataFrame(data_array, index=self.time_index, columns=column_names)
+        columns = TimeseriesId.to_multiindex(timeseries_ids)
+
+        df = pd.DataFrame(data_array, index=self.time_index, columns=columns)
 
         self.update_time_quantities(df)
 
         return df
 
-    def get_all_data_entries_and_column_names(self):
+    def get_all_data_entries_and_timeseries_ids(self):
         data_entries = self.result_data_copier.GetEmptyDataEntriesList()
-        column_names = []
+        timeseries_ids: List[TimeseriesId] = []
         for data_set in self.data.DataSets:
             data_set = impl(data_set)
             if not self.is_data_set_included(data_set):
@@ -89,7 +100,9 @@ class ResultReaderCopier(ResultReader):
                     data_entry = DataEntryNet(data_item, i)
                     data_entries.Add(data_entry)
 
-                    column_name = self.get_column_name(data_set, data_item, i)
-                    column_names.append(column_name)
+                    timeseries_id = TimeseriesId.from_dataset_dataitem_and_element(
+                        data_set, data_item, i
+                    )
+                    timeseries_ids.append(timeseries_id)
 
-        return data_entries, column_names
+        return data_entries, timeseries_ids
