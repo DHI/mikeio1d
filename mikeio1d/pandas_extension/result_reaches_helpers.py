801705fd6711f948b756e8e089e078831e61757e
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-01-23 13:08:07 +0100

    Initial custom pandas dataframe accessor implementation

---
+++ mikeio1d/pandas_extension/result_reaches_helpers.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+from .transposed_groupby import TransposedGroupBy
+
+
+def groupby_chainage(df: pd.DataFrame, **kwargs) -> TransposedGroupBy:
+    """
+    Group results for aggregation along the chainage axis.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame with results. Must have hierarchical column index (e.g. column_mode = 'all').
+
+    Returns
+    -------
+    groupby : TransposedGroupBy
+        GroupBy object, which can be used for aggregation.
+    """
+    fixed_level_names = [n for n in df.columns.names if n != "chainage"]
+    groupby = TransposedGroupBy(transposed_groupby=df.T.groupby(fixed_level_names, **kwargs))
+    return groupby
+
+
+def agg_chainage(df: pd.DataFrame, agg=["first"], gb_kwargs: dict = {}, **kwargs) -> pd.DataFrame:
+    """
+    Aggregate results along the chainage axis.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame with results. Must have hierarchical column index (e.g. column_mode = 'all').
+    agg : function, str, list or dict
+        Aggregation function(s) to apply. Same as pandas.DataFrame.agg.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        DataFrame with aggregated results.
+    """
+    groupby = groupby_chainage(df, **gb_kwargs)
+    return groupby.agg(agg, **kwargs)
---
+++ mikeio1d/pandas_extension/transposed_groupby.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+class TransposedGroupBy:
+    """
+    Same as pandas.DataFrameGroupBy, but returns the transpose of the result.
+
+    Useful where a groupby is performed on a transposed DataFrame, and after
+    aggregation the result should be transposed back.
+
+    Parameters
+    ----------
+    transposed_groupby : pandas.DataFrameGroupBy
+        A pandas.DataFrameGroupBy object, which is created from a transposed DataFrame.
+
+    Examples
+    --------
+    >>> df = res.reaches.read(column_mode='all')
+    >>> groupby = TransposedGroupBy(df.T.groupby(['quantity]))
+    >>> groupby.agg(['mean', 'max'])
+    ... # performs agg function, then returns the transpose of the result.
+    """
+
+    def __init__(self, transposed_groupby):
+        self.transposed_groupby = transposed_groupby
+
+    def __getattr__(self, name):
+        def method(*args, **kwargs):
+            result = getattr(self.transposed_groupby, name)(*args, **kwargs)
+            if isinstance(result, pd.DataFrame):
+                return result.T
+            return result
+
+        return method
