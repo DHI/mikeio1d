bd4f7b0c6d253f3c01737926ec495963818acba9
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-02-02 11:08:42 +0100

    Update ResultReaches to_geopandas() to not error with tags

--- mikeio1d/result_network/result_reaches.py
+++ mikeio1d/result_network/result_reaches.py
@@ -137,6 +137,9 @@ class ResultReaches(ResultLocations):
         df_quantities = self.read(column_mode="compact")
         df_quantities = rfa.aggregate(df_quantities)
 
+        # don't consider tags for now, only 'name'
+        df_quantities = df_quantities.groupby("name").agg(rfa.get_agg_function("time"))
+
         gdf = gdf.merge(df_quantities, left_on="name", right_index=True)
 
         return gdf
