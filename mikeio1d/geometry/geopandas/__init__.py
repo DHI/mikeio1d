4596cd77687e3e4732939a9ca301320b6ea87e47
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-02-03 09:55:56 +0100

    Add GeoPandasCatchmentsConverter and refactor ResultCatchments.to_geopandas()

--- mikeio1d/geometry/geopandas/__init__.py
+++ mikeio1d/geometry/geopandas/__init__.py
@@ -1,9 +1,11 @@
 from .geopandas_reaches_converter import GeopandasReachesConverter
 from .geopandas_reaches_converter_segmented import GeopandasReachesConverterSegmented
 from .geopandas_nodes_converter import GeoPandasNodesConverter
+from .geopandas_catchments_converter import GeoPandasCatchmentsConverter
 
 __all__ = [
     "GeopandasReachesConverter",
     "GeopandasReachesConverterSegmented",
     "GeoPandasNodesConverter",
+    "GeoPandasCatchmentsConverter",
 ]
---
+++ mikeio1d/geometry/geopandas/geopandas_catchments_converter.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from geopandas import GeoDataFrame
+
+if TYPE_CHECKING:
+    from geopandas import GeoDataFrame
+
+    from result_network import ResultCatchments
+
+
+from .geopandas_converter import GeoPandasConverter
+from ..catchment_geometry import CatchmentGeometry
+
+from mikeio1d.quantities import TimeSeriesIdGroup
+
+
+class GeoPandasCatchmentsConverter(GeoPandasConverter):
+    def __init__(self):
+        super().__init__()
+
+    def _create_dataframe_data_dict(self, catchments: ResultCatchments) -> dict[str, tuple]:
+        names = [catchment.id for catchment in catchments.values()]
+        geometries = [
+            CatchmentGeometry.from_res1d_catchment(catchment._catchment).to_shapely()
+            for catchment in catchments.values()
+        ]
+        data = {
+            "group": TimeSeriesIdGroup.CATCHMENT,
+            "name": names,
+            "geometry": geometries,
+        }
+        return data
+
+    def to_geopandas(self, catchments: ResultCatchments) -> GeoDataFrame:
+        data = self._create_dataframe_data_dict(catchments)
+        crs = self.get_crs(catchments.res1d)
+        gdf = self.gpd.GeoDataFrame(data=data, crs=crs)
+        return gdf
