a3e893b84ae55eb9c19930c95cb8f43b1d99f754
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-02-03 09:47:37 +0100

    Add GeoPandasNodesConverter and refactor ResultNodes.to_geopandas()

--- mikeio1d/geometry/geopandas/__init__.py
+++ mikeio1d/geometry/geopandas/__init__.py
@@ -1,4 +1,9 @@
 from .geopandas_reaches_converter import GeopandasReachesConverter
 from .geopandas_reaches_converter_segmented import GeopandasReachesConverterSegmented
+from .geopandas_nodes_converter import GeoPandasNodesConverter
 
-__all__ = ["GeopandasReachesConverter", "GeopandasReachesConverterSegmented"]
+__all__ = [
+    "GeopandasReachesConverter",
+    "GeopandasReachesConverterSegmented",
+    "GeoPandasNodesConverter",
+]
---
+++ mikeio1d/geometry/geopandas/geopandas_nodes_converter.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from geopandas import GeoDataFrame
+
+if TYPE_CHECKING:
+    from geopandas import GeoDataFrame
+
+    from result_network import ResultNodes
+
+
+from .geopandas_converter import GeoPandasConverter
+from ..node_point import NodePoint
+
+from mikeio1d.quantities import TimeSeriesIdGroup
+
+
+class GeoPandasNodesConverter(GeoPandasConverter):
+    def __init__(self):
+        super().__init__()
+
+    def _create_dataframe_data_dict(self, nodes: ResultNodes) -> dict[str, tuple]:
+        names = [node.id for node in nodes.values()]
+        geometries = [NodePoint.from_res1d_node(node._node).to_shapely() for node in nodes.values()]
+        data = {
+            "group": TimeSeriesIdGroup.NODE,
+            "name": names,
+            "geometry": geometries,
+        }
+        return data
+
+    def to_geopandas(self, nodes: ResultNodes) -> GeoDataFrame:
+        data = self._create_dataframe_data_dict(nodes)
+        crs = self.get_crs(nodes.res1d)
+        gdf = self.gpd.GeoDataFrame(data=data, crs=crs)
+        return gdf
