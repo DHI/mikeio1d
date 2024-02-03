4bd5b3494e7872ebf7f954caf947033fec2bcebe
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-02-03 09:19:51 +0100

    Add GeoPandasReachesConverterSegmented and refactor ResultReaches.to_geopandas()

---
+++ mikeio1d/geometry/geopandas/geopandas_reaches_converter_segmented.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from geopandas import GeoDataFrame
+
+if TYPE_CHECKING:
+    from geopandas import GeoDataFrame
+    from mikeio1d.result_network import ResultReaches
+
+from .geopandas_converter import GeoPandasConverter
+from ..reach_geometry import ReachGeometry
+
+from mikeio1d.quantities import TimeSeriesId
+from mikeio1d.quantities import TimeSeriesIdGroup
+
+
+class GeopandasReachesConverterSegmented(GeoPandasConverter):
+    def __init__(self):
+        super().__init__()
+
+    def _create_dataframe_data_dict(self, reaches: ResultReaches) -> dict[str, tuple]:
+        data = {
+            "group": [],
+            "name": [],
+            "tag": [],
+            "geometry": [],
+        }
+        for reach in reaches.values():
+            for m1d_reach in reach.reaches:
+                data["group"].append(TimeSeriesIdGroup.REACH)
+                data["name"].append(reach.name)
+                data["tag"].append(TimeSeriesId.create_reach_span_tag(m1d_reach))
+                data["geometry"].append(ReachGeometry.from_m1d_reaches(m1d_reach).to_shapely())
+        return data
+
+    def to_geopandas(self, reaches: ResultReaches) -> GeoDataFrame:
+        data = self._create_dataframe_data_dict(reaches)
+        crs = self.get_crs(reaches.res1d)
+        gdf = self.gpd.GeoDataFrame(data=data, crs=crs)
+        return gdf
