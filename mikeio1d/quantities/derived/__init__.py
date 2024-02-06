88ba513d726d77759795bbc4d887f275ac08b099
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-02-06 14:45:05 +0100

    Add DerivedQuantityManager for registering/deregistering new DerivesQuantity classes

--- mikeio1d/quantities/derived/__init__.py
+++ mikeio1d/quantities/derived/__init__.py
@@ -1,3 +1,4 @@
 from .derived_quantity import DerivedQuantity
+from .derived_quantity_manager import derived_quantity_manager
 
-__all__ = ["DerivedQuantity"]
+__all__ = ["DerivedQuantity", "derived_quantity_manager"]
