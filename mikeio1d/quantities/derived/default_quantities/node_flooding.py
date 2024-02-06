1c7ba1c0cb420c3d30199032e3aadc8d13455131
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-02-06 14:43:49 +0100

    Add NodeFlooding derived quantity

---
+++ mikeio1d/quantities/derived/default_quantities/node_flooding.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List
+
+    import pandas as pd
+
+    from mikeio1d.result_network import ResultLocation
+
+from ..derived_quantity import DerivedQuantity
+from ...timeseries_id import TimeSeriesIdGroup
+
+import numpy as np
+
+
+class NodeFlooding(DerivedQuantity):
+    _NAME = "NodeFlooding"
+    _GROUPS = {TimeSeriesIdGroup.NODE}
+    _SOURCE_QUANTITY = "WaterLevel"
+
+    def derive(self, df_source: pd.DataFrame, locations: List[ResultLocation]) -> pd.DataFrame:
+
+        ground_level = [self.get_ground_level(location) for location in locations]
+        df_derived = df_source - ground_level
+
+        return df_derived
+
+    def get_ground_level(self, location: ResultLocation):
+        return getattr(location, "ground_level", np.nan)
