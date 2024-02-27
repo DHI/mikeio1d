ef271c81d7ff971716d7db911c88b326eb8eeeb4
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-02-27 13:56:05 +0100

    Ability to set/unset markers in different ways.

--- mikeio1d/cross_sections/__init__.py
+++ mikeio1d/cross_sections/__init__.py
@@ -1,4 +1,5 @@
 from .cross_section import CrossSection
+from .cross_section import Marker
 from .cross_section_collection import CrossSectionCollection
 
-__all__ = ["CrossSection", "CrossSectionCollection"]
+__all__ = ["CrossSection", "CrossSectionCollection", "Marker"]
