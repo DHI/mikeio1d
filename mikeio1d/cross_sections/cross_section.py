a83f9cd1be082b9748059a3ebbfee6c942583c46
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-03-01 08:44:28 +0100

    Clean up imports

--- mikeio1d/cross_sections/cross_section.py
+++ mikeio1d/cross_sections/cross_section.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
+from warnings import warn
 
 from typing import TYPE_CHECKING
-from warnings import warn
 
 if TYPE_CHECKING:
     from typing import Iterable
@@ -10,63 +10,19 @@ if TYPE_CHECKING:
     from ..geometry import CrossSectionGeometry
 
 import numpy as np
-
-from enum import Enum
-
 import pandas as pd
-
 import matplotlib.pyplot as plt
 
-from ..various import try_import_shapely
 from .cross_section_factory import CrossSectionFactory
+from .marker import Marker
+
+from ..various import try_import_shapely
 
 import System
 from DHI.Mike1D.CrossSectionModule import CrossSectionPoint
 from DHI.Mike1D.Generic import ProcessingOption
 from DHI.Mike1D.Generic import ResistanceDistribution
 from DHI.Mike1D.Generic import ResistanceFormulation
-
-
-class Marker(Enum):
-    LEFT_LEVEE_BANK = 1
-    LOWEST_POINT = 2
-    RIGHT_LEVEE_BANK = 3
-    LEFT_LOW_FLOW_BANK = 4
-    RIGHT_LOW_FLOW_BANK = 5
-
-    def __repr__(self) -> str:
-        return Marker.pretty(self)
-
-    def __int__(self) -> int:
-        return self.value
-
-    @staticmethod
-    def is_default_marker(marker: int | Marker) -> bool:
-        return marker in Marker
-
-    @staticmethod
-    def is_user_marker(marker: int | Marker) -> bool:
-        MIN_USER_MARKER = 8
-        return marker >= MIN_USER_MARKER
-
-    @staticmethod
-    def pretty(marker: int | Marker) -> str:
-        if Marker.is_default_marker(marker):
-            marker = marker if isinstance(marker, Marker) else Marker(marker)
-            return marker.name.replace("_", " ").title() + f" ({marker.value})"
-        elif Marker.is_user_marker(marker):
-            return f"User Marker ({marker})"
-        else:
-            return f"Unknown Marker ({marker})"
-
-    @staticmethod
-    def from_pretty(marker: str) -> int:
-        marker = int(marker.split("(")[-1][:-1])
-        return marker
-
-    @staticmethod
-    def list_from_string(s: str) -> List[int]:
-        return [int(m) for m in s.split(",")]
 
 
 class CrossSection:
