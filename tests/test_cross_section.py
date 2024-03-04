62f20df3ccedcc68953aef27ce7330347ab31b95
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-03-04 10:49:02 +0100

    Add more tests and enums for ResistanceType and ResistanceDistribution

--- tests/test_cross_section.py
+++ tests/test_cross_section.py
@@ -3,6 +3,8 @@ import pytest
 import numpy as np
 
 from mikeio1d.cross_sections import CrossSection
+from mikeio1d.cross_sections import ResistanceType
+from mikeio1d.cross_sections import ResistanceDistribution
 
 
 def create_xz_data():
@@ -16,6 +18,14 @@ def xz_data():
     return create_xz_data()
 
 
+@pytest.fixture
+def dummy_cs(xz_data):
+    x, z = xz_data
+    return CrossSection.from_xz(
+        x=x, z=z, location_id="my_location", chainage=100, topo_id="my_topo"
+    )
+
+
 class TestCrossSectionCreation:
     def test_create_cross_section(self, xz_data):
         x, z = xz_data
@@ -23,6 +33,54 @@ class TestCrossSectionCreation:
             x=x, z=z, location_id="my_location", chainage=100, topo_id="my_topo"
         )
         assert isinstance(cs, CrossSection)
-        assert cs.location_id == "my_location"
-        assert pytest.approx(cs.chainage) == 100
-        assert cs.topo_id == "my_topo"
+
+    def test_topo_id(self, dummy_cs):
+        assert dummy_cs.topo_id == "my_topo"
+
+    def test_location_id(self, dummy_cs):
+        assert dummy_cs.location_id == "my_location"
+
+    def test_chainage(self, dummy_cs):
+        assert dummy_cs.chainage == 100
+
+    def test_bottom_level(self, dummy_cs):
+        assert dummy_cs.bottom_level == 10
+
+    def test_height(self, dummy_cs):
+        assert dummy_cs.height == 10
+
+    def test_interpolate(self, dummy_cs):
+        assert dummy_cs.interpolated is False
+
+    def test_is_open(self, dummy_cs):
+        assert dummy_cs.is_open is True
+
+    def test_max_width(self, dummy_cs):
+        assert dummy_cs.max_width == 100
+
+    def test_min_water_depth(self, dummy_cs):
+        assert dummy_cs.min_water_depth == 0
+
+    def test_zmax(self, dummy_cs):
+        assert dummy_cs.zmax == 20
+
+    def test_zmin(self, dummy_cs):
+        assert dummy_cs.zmin == 10
+
+    def test_geometry(self, dummy_cs):
+        # TODO
+        assert False
+
+    def test_resistance_type_get(self, dummy_cs):
+        assert dummy_cs.resistance_type == ResistanceType.RELATIVE
+
+    def test_resistance_type_set(self, dummy_cs):
+        dummy_cs.resistance_type = ResistanceType.MANNINGS_N
+        assert dummy_cs.resistance_type == ResistanceType.MANNINGS_N
+
+    def test_resistance_distribution_get(self, dummy_cs):
+        assert dummy_cs.resistance_distribution == ResistanceDistribution.UNIFORM
+
+    def test_resistance_distribution_set(self, dummy_cs):
+        dummy_cs.resistance_distribution = ResistanceDistribution.ZONES
+        assert dummy_cs.resistance_distribution == ResistanceDistribution.ZONES
