87dc4bc5290faff922b05b6bd860b2d66e53a2d1
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-03-04 11:57:09 +0100

    Add more tests

--- tests/test_cross_section.py
+++ tests/test_cross_section.py
@@ -1,10 +1,17 @@
 import pytest
 
+from typing import List
+from contextlib import nullcontext
+
 import numpy as np
+
+from mikeio1d import Xns11
 
 from mikeio1d.cross_sections import CrossSection
 from mikeio1d.cross_sections import ResistanceType
 from mikeio1d.cross_sections import ResistanceDistribution
+
+from tests import testdata
 
 
 def create_xz_data():
@@ -19,11 +26,28 @@ def xz_data():
 
 
 @pytest.fixture
-def dummy_cs(xz_data):
+def xns_basic():
+    return Xns11(testdata.xsections_xns11)
+
+
+@pytest.fixture
+def cs_dummy(xz_data) -> CrossSection:
     x, z = xz_data
     return CrossSection.from_xz(
         x=x, z=z, location_id="my_location", chainage=100, topo_id="my_topo"
     )
+
+
+@pytest.fixture
+def cs_basic(xns_basic) -> CrossSection:
+    return xns_basic.xsections.values()[0]
+
+
+@pytest.fixture
+def cs_sample(xns_basic) -> List[CrossSection]:
+    x = list(xns_basic.xsections.values())
+    x = x[::2]
+    return x
 
 
 class TestCrossSectionCreation:
@@ -34,53 +58,89 @@ class TestCrossSectionCreation:
         )
         assert isinstance(cs, CrossSection)
 
-    def test_topo_id(self, dummy_cs):
-        assert dummy_cs.topo_id == "my_topo"
+    def test_topo_id(self, cs_dummy):
+        assert cs_dummy.topo_id == "my_topo"
 
-    def test_location_id(self, dummy_cs):
-        assert dummy_cs.location_id == "my_location"
+    def test_location_id(self, cs_dummy):
+        assert cs_dummy.location_id == "my_location"
 
-    def test_chainage(self, dummy_cs):
-        assert dummy_cs.chainage == 100
+    def test_chainage(self, cs_dummy):
+        assert cs_dummy.chainage == 100
 
-    def test_bottom_level(self, dummy_cs):
-        assert dummy_cs.bottom_level == 10
+    def test_bottom_level(self, cs_dummy):
+        assert cs_dummy.bottom_level == 10
 
-    def test_height(self, dummy_cs):
-        assert dummy_cs.height == 10
+    def test_height(self, cs_dummy):
+        assert cs_dummy.height == 10
 
-    def test_interpolate(self, dummy_cs):
-        assert dummy_cs.interpolated is False
+    def test_interpolate(self, cs_dummy):
+        assert cs_dummy.interpolated is False
 
-    def test_is_open(self, dummy_cs):
-        assert dummy_cs.is_open is True
+    def test_is_open(self, cs_dummy):
+        assert cs_dummy.is_open is True
 
-    def test_max_width(self, dummy_cs):
-        assert dummy_cs.max_width == 100
+    def test_max_width(self, cs_dummy):
+        assert cs_dummy.max_width == 100
 
-    def test_min_water_depth(self, dummy_cs):
-        assert dummy_cs.min_water_depth == 0
+    def test_min_water_depth(self, cs_dummy):
+        assert cs_dummy.min_water_depth == 0
 
-    def test_zmax(self, dummy_cs):
-        assert dummy_cs.zmax == 20
+    def test_zmax(self, cs_dummy):
+        assert cs_dummy.zmax == 20
 
-    def test_zmin(self, dummy_cs):
-        assert dummy_cs.zmin == 10
+    def test_zmin(self, cs_dummy):
+        assert cs_dummy.zmin == 10
 
-    def test_geometry(self, dummy_cs):
+    @pytest.mark.skip
+    def test_geometry(self, cs_dummy):
         # TODO
         assert False
 
-    def test_resistance_type_get(self, dummy_cs):
-        assert dummy_cs.resistance_type == ResistanceType.RELATIVE
+    def test_resistance_type_get(self, cs_dummy):
+        assert cs_dummy.resistance_type == ResistanceType.RELATIVE
 
-    def test_resistance_type_set(self, dummy_cs):
-        dummy_cs.resistance_type = ResistanceType.MANNINGS_N
-        assert dummy_cs.resistance_type == ResistanceType.MANNINGS_N
+    def test_resistance_type_set(self, cs_dummy):
+        cs_dummy.resistance_type = ResistanceType.MANNINGS_N
+        assert cs_dummy.resistance_type == ResistanceType.MANNINGS_N
 
-    def test_resistance_distribution_get(self, dummy_cs):
-        assert dummy_cs.resistance_distribution == ResistanceDistribution.UNIFORM
+    def test_resistance_distribution_get(self, cs_dummy):
+        assert cs_dummy.resistance_distribution == ResistanceDistribution.UNIFORM
 
-    def test_resistance_distribution_set(self, dummy_cs):
-        dummy_cs.resistance_distribution = ResistanceDistribution.ZONES
-        assert dummy_cs.resistance_distribution == ResistanceDistribution.ZONES
+    def test_resistance_distribution_set(self, cs_dummy):
+        cs_dummy.resistance_distribution = ResistanceDistribution.ZONES
+        assert cs_dummy.resistance_distribution == ResistanceDistribution.ZONES
+
+    @pytest.mark.parametrize(
+        "zone", ["resistance_left_high_flow", "resistance_right_high_flow", "resistance_low_flow"]
+    )
+    @pytest.mark.parametrize("get_or_set", ["get", "set"])
+    def test_resistance_zones_warning(self, cs_sample, zone, get_or_set):
+        """
+        Warns when using zone related resistances with non-zone resistance distribution
+        """
+        tested_one_zone_cross_section = False
+        for cs in cs_sample:
+            should_warn = cs.resistance_distribution != ResistanceDistribution.ZONES
+            tested_one_zone_cross_section = tested_one_zone_cross_section or not should_warn
+            with pytest.warns() if should_warn else nullcontext():
+                if get_or_set == "get":
+                    getattr(cs, zone)
+                else:
+                    setattr(cs, zone, 2.0)
+
+        assert tested_one_zone_cross_section is True
+
+    @pytest.mark.parametrize(
+        "zone", ["resistance_left_high_flow", "resistance_right_high_flow", "resistance_low_flow"]
+    )
+    def test_resistance_zones_get(self, cs_dummy, zone):
+        cs_dummy.resistance_distribution = ResistanceDistribution.ZONES
+        assert getattr(cs_dummy, zone) == 1.0
+
+    @pytest.mark.parametrize(
+        "zone", ["resistance_left_high_flow", "resistance_right_high_flow", "resistance_low_flow"]
+    )
+    def test_resistance_zones_set(self, cs_dummy, zone):
+        cs_dummy.resistance_distribution = ResistanceDistribution.ZONES
+        setattr(cs_dummy, zone, 2.0)
+        assert getattr(cs_dummy, zone) == 2.0
