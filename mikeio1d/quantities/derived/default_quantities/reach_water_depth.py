78c75ad54e1d402426b4e769167610a154add5ea
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-09-16 12:52:59 +0200

    Add reach water depth as default derived quantity

---
+++ mikeio1d/quantities/derived/default_quantities/reach_water_depth.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List
+
+    import pandas as pd
+
+    from mikeio1d.result_network import ResultLocation
+
+from ..derived_quantity import DerivedQuantity
+from ...timeseries_id import TimeSeriesIdGroup
+
+
+class ReachWaterDepth(DerivedQuantity):
+    _NAME = "ReachWaterDepth"
+    _GROUPS = {TimeSeriesIdGroup.REACH}
+    _SOURCE_QUANTITY = "WaterLevel"
+
+    def derive(self, df_source: pd.DataFrame, locations: List[ResultLocation]):
+        bottom_levels = tuple(gridpoint.bottom_level for gridpoint in locations)
+        df_derived = df_source - bottom_levels
+        return df_derived
