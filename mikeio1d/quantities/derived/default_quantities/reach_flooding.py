6aee72fec41523dfc6606064374272e1e14ba637
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-09-16 15:09:42 +0200

    Add reach flooding as default derived quantity

---
+++ mikeio1d/quantities/derived/default_quantities/reach_flooding.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List
+
+    import pandas as pd
+
+    from mikeio1d.result_network import ResultLocation
+
+from ..derived_quantity import DerivedQuantity
+from ...timeseries_id import TimeSeriesIdGroup
+
+
+class ReachFlooding(DerivedQuantity):
+    _NAME = "ReachFlooding"
+    _GROUPS = {TimeSeriesIdGroup.REACH}
+    _SOURCE_QUANTITY = "WaterLevel"
+
+    def derive(self, df_source: pd.DataFrame, locations: List[ResultLocation]):
+        ground_levels = tuple(
+            gridpoint.result_reach.interpolate_reach_ground_level(gridpoint.chainage)
+            for gridpoint in locations
+        )
+        df_derived = df_source - ground_levels
+        return df_derived
