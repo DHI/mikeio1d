39e6e847ac30f5baaa0e69b945c068482e6a1a08
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-09-17 10:40:17 +0200

    Add qqmanning as a default derived quantity

---
+++ mikeio1d/quantities/derived/default_quantities/reach_qq_manning.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List
+
+    import pandas as pd
+
+    from mikeio1d.result_network import ResultLocation
+
+from ..derived_quantity import DerivedQuantity
+from ...timeseries_id import TimeSeriesIdGroup
+
+
+class ReachQQManning(DerivedQuantity):
+    _NAME = "ReachQQManning"
+    _GROUPS = {TimeSeriesIdGroup.REACH}
+    _SOURCE_QUANTITY = "Discharge"
+
+    def derive(self, df_source: pd.DataFrame, locations: List[ResultLocation]):
+        full_flow_discharges = tuple(
+            gridpoint.result_reach.full_flow_discharge for gridpoint in locations
+        )
+        df_derived = df_source / full_flow_discharges
+        return df_derived
