1380f9db250cf2be4b50ba5720cd8b6dfc998eb2
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-09-17 16:39:41 +0200

    Add tests for new derived quantities

--- tests/test_derived_quantities.py
+++ tests/test_derived_quantities.py
@@ -135,3 +135,105 @@ class TestSpecificDerivedQuantities:
         assert df.max().max() == -np.inf
         assert df.min().min() == -np.inf
         assert df.mean().mean() == -np.inf
+
+    def test_reach_flooding(self, res1d_network):
+        df = res1d_network.reaches["84l1"].ReachFlooding.read()
+
+        # (lower chainage bound, upper chainage bound, agg) : expected_value
+        data = {
+            (96, 97, "max"): -0.775863647460938,
+            (48, 49, "max"): -0.532844543457031,
+            (0, 1, "max"): -0.290130615234375,
+            (96, 97, "min"): -3.42999267578125,
+            (48, 49, "min"): -3.15599822998047,
+            (0, 1, "min"): -2.87599182128906,
+            (96, 97, "mean"): -2.83537195379084,
+            (48, 49, "mean"): -2.6148774580522,
+            (0, 1, "mean"): -2.38141729181463,
+        }
+
+        for (lower, upper, agg), expected_value in data.items():
+            assert df.m1d.query(f"chainage.between({lower},{upper})").agg(agg).agg(
+                agg
+            ) == pytest.approx(expected_value, abs=1e-4)
+
+    def test_reach_depth(self, res1d_network):
+        df = res1d_network.reaches["84l1"].ReachWaterDepth.read()
+
+        # (lower chainage bound, upper chainage bound, agg) : expected_value
+        data = {
+            (96, 97, "max"): 2.66413879394531,
+            (48, 49, "max"): 2.62715148925781,
+            (0, 1, "max"): 2.58985900878906,
+            (96, 97, "min"): 0.010009765625,
+            (48, 49, "min"): 0.003997802734375,
+            (0, 1, "min"): 0.003997802734375,
+            (96, 97, "mean"): 0.604630487615412,
+            (48, 49, "mean"): 0.545118574662642,
+            (0, 1, "mean"): 0.498572332208807,
+        }
+
+        for (lower, upper, agg), expected_value in data.items():
+            assert df.m1d.query(f"chainage.between({lower},{upper})").agg(agg).agg(
+                agg
+            ) == pytest.approx(expected_value, abs=1e-4)
+
+    def test_reach_water_level_above_critical(self, res1d_network):
+        df = res1d_network.reaches["84l1"].ReachWaterLevelAboveCritical.read()
+
+        # (lower chainage bound, upper chainage bound, agg) : expected_value
+        data = {
+            (96, 97, "max"): -0.775863647460938,
+            (48, 49, "max"): -0.532844543457031,
+            (0, 1, "max"): -0.290130615234375,
+            (96, 97, "min"): -3.42999267578125,
+            (48, 49, "min"): -3.15599822998047,
+            (0, 1, "min"): -2.87599182128906,
+            (96, 97, "mean"): -2.83537195379084,
+            (48, 49, "mean"): -2.6148774580522,
+            (0, 1, "mean"): -2.38141729181463,
+        }
+
+        for (lower, upper, agg), expected_value in data.items():
+            assert df.m1d.query(f"chainage.between({lower},{upper})").agg(agg).agg(
+                agg
+            ) == pytest.approx(expected_value, abs=1e-4)
+
+    def test_reach_filling(self, res1d_network):
+        df = res1d_network.reaches["84l1"].ReachFilling.read()
+
+        # (lower chainage bound, upper chainage bound, agg) : expected_value
+        data = {
+            (96, 97, "max"): 3.33017349243164,
+            (48, 49, "max"): 3.28393936157227,
+            (0, 1, "max"): 3.23732376098633,
+            (96, 97, "min"): 0.01251220703125,
+            (48, 49, "min"): 0.00499725341796875,
+            (0, 1, "min"): 0.00499725341796875,
+            (96, 97, "mean"): 0.755788109519265,
+            (48, 49, "mean"): 0.681398218328303,
+            (0, 1, "mean"): 0.623215415261009,
+        }
+
+        for (lower, upper, agg), expected_value in data.items():
+            assert df.m1d.query(f"chainage.between({lower},{upper})").agg(agg).agg(
+                agg
+            ) == pytest.approx(expected_value, abs=1e-4)
+
+    def test_reach_qq_manning(self, res1d_network):
+        df = res1d_network.reaches["84l1"].ReachQQManning.read()
+
+        # (lower chainage bound, upper chainage bound, agg) : expected_value
+        data = {
+            (24, 25, "max"): 0.899804353713989,
+            (72, 73, "max"): 0.884586334228516,
+            (24, 25, "min"): 6.45247928332537e-05,
+            (72, 73, "min"): -0.00437665591016412,
+            (24, 25, "mean"): 0.17750987808699,
+            (72, 73, "mean"): 0.17502574035964,
+        }
+
+        for (lower, upper, agg), expected_value in data.items():
+            assert df.m1d.query(f"chainage.between({lower},{upper})").agg(agg).agg(
+                agg
+            ) == pytest.approx(expected_value, abs=1e-4)
