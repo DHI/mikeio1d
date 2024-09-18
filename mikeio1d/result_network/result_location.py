763dd06c707f0d2e6742f96640112bb309a83155
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-09-18 12:49:32 +0200

    Exclude type checking from test coverage

--- mikeio1d/result_network/result_location.py
+++ mikeio1d/result_network/result_location.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from typing import Dict
     from typing import List
     from typing import Optional
