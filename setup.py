58264105b6d155ad2e778cb86b3833b7e89bee3a
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-09-19 08:08:26 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mikeio1d",
-    version="0.6.1",
+    version="0.7.0",
     install_requires=[
         "pythonnet>=3.0.0",
         "numpy",
