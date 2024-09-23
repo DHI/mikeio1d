a37642af4927bd882cedbcae4ac6f1920c2000f0
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-09-23 11:40:30 +0200

    Update docs to quarto. Also update documentation

--- mikeio1d/result_network/__init__.py
+++ mikeio1d/result_network/__init__.py
@@ -7,6 +7,23 @@ from .result_network import ResultNetwork
 from .result_node import ResultNode
 from .result_nodes import ResultNodes
 from .result_quantity import ResultQuantity
+from .result_quantity_collection import ResultQuantityCollection
+from .result_global_data import ResultGlobalData
+from .result_global_datas import ResultGlobalDatas
 from .result_reach import ResultReach
 from .result_reaches import ResultReaches
 from .various import make_proper_variable_name
+
+__all__ = [
+    "ResultCatchment",
+    "ResultCatchments",
+    "ResultNode",
+    "ResultNodes",
+    "ResultReach",
+    "ResultReaches",
+    "ResultGridPoint",
+    "ResultGlobalData",
+    "ResultGlobalDatas",
+    "ResultQuantity",
+    "ResultQuantityCollection",
+]
