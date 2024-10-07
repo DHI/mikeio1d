ac6fd408204f18dc24ad519707bd43229580fca8
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-10-07 10:22:02 +0200

    Refactor: simplify derived quantity long lines

--- mikeio1d/quantities/derived/default_quantities/reach_water_level_above_critical.py
+++ mikeio1d/quantities/derived/default_quantities/reach_water_level_above_critical.py
@@ -8,6 +8,9 @@ if TYPE_CHECKING:  # pragma: no cover
     import pandas as pd
 
     from mikeio1d.result_network import ResultLocation
+    from mikeio1d.result_network import ResultGridPoint
+
+import numpy as np
 
 from ..derived_quantity import DerivedQuantity
 from ...timeseries_id import TimeSeriesIdGroup
@@ -19,9 +22,13 @@ class ReachWaterLevelAboveCritical(DerivedQuantity):
     _SOURCE_QUANTITY = "WaterLevel"
 
     def derive(self, df_source: pd.DataFrame, locations: List[ResultLocation]):
-        critical_levels = tuple(
-            gridpoint.result_reach.interpolate_reach_critical_level(gridpoint.chainage)
-            for gridpoint in locations
-        )
+        dtype = df_source.dtypes[0]
+        critical_levels = np.fromiter(self.get_critical_levels(locations), dtype=dtype)
         df_derived = df_source - critical_levels
         return df_derived
+    
+    def get_critical_level(self, gridpoint: ResultGridPoint):
+        return gridpoint.result_reach.interpolate_reach_critical_level(gridpoint.chainage)
+    
+    def get_critical_levels(self, gridpoints: List[ResultGridPoint]):
+        yield from (self.get_critical_level(location) for location in gridpoints)
