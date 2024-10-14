5e018ae65eb8a8bd54678ca39c8f9efc709d5cd8
Ryan Murray <74630349+rywm-dhi@users.noreply.github.com>
2024-10-14 14:21:50 +0200

    Fix wheel platform tags

--- scripts/hatch_build_script.py
+++ scripts/hatch_build_script.py
@@ -1,4 +1,8 @@
 import sys
+<<<<<<< Updated upstream
+=======
+import sysconfig
+>>>>>>> Stashed changes
 import os
 
 sys.path.insert(0, os.path.dirname(__file__))
@@ -9,4 +13,36 @@ from install_dependencies import main
 
 class BuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
+<<<<<<< Updated upstream
         main()
+=======
+        main()
+        self.update_build_data(build_data)
+
+    def update_build_data(self, build_data):
+        tag = build_data.get("tag", None)
+        build_data["tag"] = self.update_tag_platform(tag)
+
+    def update_tag_platform(self, tag: str | None) -> str:
+        DEFAULT_TAG = "py3-none-any"
+        if tag is None:
+            tag = DEFAULT_TAG 
+        platform = self._get_platform()
+        return self._replace_platform_part_of_tag(tag, platform)
+
+    def _replace_platform_part_of_tag(self, tag: str, platform: str):
+        TAG_DELIMITER = "-"
+        tag = tag.split(TAG_DELIMITER)
+        tag[-1] = platform
+        return TAG_DELIMITER.join(tag)
+
+    def _get_platform(self):
+        platform = sysconfig.get_platform()
+        if platform.startswith("linux"):
+            return "manylinux2010_x86_64"
+        elif platform.startswith("win"):
+            return "win_amd64"
+        else:
+            raise Exception(f"Unsupported platform: {platform}")
+        
+>>>>>>> Stashed changes
